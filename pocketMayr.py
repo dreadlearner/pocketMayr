7067ae28f4bce1c8f831bb88dcbca1985f412532
dreadlearner <79397409+dreadlearner@users.noreply.github.com>
2021-05-14 13:17:29 -0400

    google doc note links

--- pocketMayr.py
+++ pocketMayr.py
@@ -128,6 +128,10 @@ while True:
                 videolinks = ["https://www.youtube.com/watch?v=2bWyFYcQQME", "https://www.youtube.com/watch?v=6pHHJqUDpDg", "https://www.youtube.com/watch?v=Zh_syCs0Pz8"]
                 subject = [units.unit1.video1.captions, units.unit1.video2.captions, units.unit1.video3.captions]
                 note = [notesimport.unit1.ki11, notesimport.unit1.ki12, notesimport.unit1.ki13, notesimport.unit1.ki14]
+                notelinks = ['https://docs.google.com/document/d/1yJkSumMR0N5W5bXYWRzeQiqYMFlzXBUosOMBYKVJh_g/edit?usp=sharing',
+                'https://docs.google.com/document/d/1574t6LyOoxUxia8QC8YQXbQCepHltuxU2SLE7RAaybI/edit?usp=sharing',
+                'https://docs.google.com/document/d/1nR5iFWasmllHfrN6SQ5fKID_uInvCCD25PZs_JcMszU/edit?usp=sharing',
+                'https://docs.google.com/document/d/1GQZwljULbwgpAFQZ9I-dx2VmhRZ_PRfnfokZk5M1kBI/edit?usp=sharing']
                 timeperiod =  '0 - 1607'
             if unit == 'unit2':
                 videolinks = ["https://www.youtube.com/watch?v=LWfcXHMLaHU", "https://www.youtube.com/watch?v=CVy_9FhVfKE", "https://www.youtube.com/watch?v=kKL1UV5AiW0",
@@ -136,6 +140,11 @@ while True:
                 subject = [units.unit2.video1.captions, units.unit2.video2.captions, units.unit2.video3.captions, units.unit2.video4.captions, units.unit2.video5.captions,
                 units.unit2.video6.captions, units.unit2.video7.captions]
                 note = [notesimport.unit2.ki21, notesimport.unit2.ki22, notesimport.unit2.ki23, notesimport.unit2.ki24, notesimport.unit2.ki25]
+                notelinks = ['https://docs.google.com/document/d/1ptnT4Un3fbXoUw4raZTrGKS8qB5uoXmTgp-TqPaQJ38/edit?usp=sharing',
+                'https://docs.google.com/document/d/10l_6ur4FxHHhBWXg6hY0kUekyyO0Eo-4m939vhGmXoA/edit?usp=sharing',
+                'https://docs.google.com/document/d/1OXW-ulmLNgpVMpsdnw13hikSy2BpjkCEFo8pid7-cIc/edit?usp=sharing',
+                'https://docs.google.com/document/d/1bra_wH_3vnEOolvdgMUv9FAo1L3kjGS5pRJJ8BfPR4A/edit?usp=sharing',
+                'https://docs.google.com/document/d/1yF6q9eHoYZn84szdKnrj-70nE0tdqhAjGgMflGZAB4A/edit?usp=sharing']
                 timeperiod =  '1607 - 1754'
             if unit == 'unit3':
                 videolinks = ["https://youtu.be/5ty4aUqQXtI", "https://www.youtube.com/watch?v=eMEk3cVAy7s", "https://www.youtube.com/watch?v=4G_-6u_2A6I",
@@ -143,6 +152,13 @@ while True:
                 subject = [units.unit3.video1.captions, units.unit3.video2.captions, units.unit3.video3.captions, units.unit3.video4.captions, units.unit3.video5.captions,
                 units.unit3.video6.captions]
                 note = [notesimport.unit3.ki31, notesimport.unit3.ki32, notesimport.unit3.ki33, notesimport.unit3.ki34, notesimport.unit3.ki35, notesimport.unit3.ki36, notesimport.unit3.ki37]
+                notelinks = ['https://docs.google.com/document/d/1so3fM3nX4sUFfuOilskk5w8SuChKx9Y_JtJ8WmwOVeY/edit?usp=sharing',
+                'https://docs.google.com/document/d/1_RofzPY2EVGzz_4iC_70Gde1kWkZKnkcli8uPlBXmvM/edit?usp=sharing',
+                'https://docs.google.com/document/d/14VBxfyMc8xgel80V0AxLh-DFNDA4vCImEyffonzZFA8/edit?usp=sharing',
+                'https://docs.google.com/document/d/1vJLsWhIW7ZlJI7a22SZtGIvW_bjd_A2GCQmtvFiJkoA/edit?usp=sharing',
+                'https://docs.google.com/document/d/15fc-wl3Ao3qX1HMbIIA13oVv4RKEY6CUULuSWdiKSgA/edit?usp=sharing',
+                'https://docs.google.com/document/d/1LXO2uF0V5xEQay-8bGDFrj9gRHs2hxSR4gE6k6D2wyM/edit?usp=sharing',
+                'https://docs.google.com/document/d/1ppaRIjqA7m7tMLG-vAfVVbMjXeKxE2VmDqQunamqddo/edit?usp=sharing']
                 timeperiod =  '1754 - 1800'
             if unit == 'unit4':
                 videolinks = ["https://www.youtube.com/watch?v=Xtl_f54uOEk", "https://www.youtube.com/watch?v=pgzSTdB7aTM&feature=emb_imp_woyt", "https://www.youtube.com/watch?v=Z_Af0G9_TdU",
@@ -151,6 +167,12 @@ while True:
                 subject = [units.unit4.video1.captions, units.unit4.video2.captions, units.unit4.video3.captions, units.unit4.video4.captions, units.unit4.video5.captions,
                 units.unit4.video6.captions, units.unit4.video7.captions, units.unit4.video8.captions]
                 note = [notesimport.unit4.ki41, notesimport.unit4.ki42, notesimport.unit4.ki43, notesimport.unit4.ki44, notesimport.unit4.ki45, notesimport.unit4.ki46]
+                notelinks = ["https://docs.google.com/document/d/1kbIgm7E5E_chErNSFG8FjgVJx6AdsXbbfNvKEJ4jB70/edit?usp=sharing",
+                "https://docs.google.com/document/d/1XLDr2CXXjds0Fj1bM7SytN3r19adzlQ6a70g99iM7_E/edit?usp=sharing",
+                "https://docs.google.com/document/d/1peFwWGRXESCH7lLoGQZmHBAfDdSzf8kb0CJQxO1GEZ8/edit?usp=sharing",
+                "https://docs.google.com/document/d/1DzS-n04NOr0Ye6wN7N29pWi5n0XPdGbVECFKvpbPUgY/edit?usp=sharing",
+                "https://docs.google.com/document/d/1SUotMoNGULOF80EfspdazgfIryqAqjZz9zGrrxfQFD8/edit?usp=sharing",
+                "https://docs.google.com/document/d/1eCxehxuXJZ6YgkSVzP1T5tvhyCatRnPrMjojzUETwuo/edit?usp=sharing"]
                 timeperiod =  '1800 - 1848'
             if unit == 'unit5':
                 videolinks = ["https://www.youtube.com/watch?v=CIOgaUcd1n0", "https://www.youtube.com/watch?v=V7YwXGrF5p8", "https://www.youtube.com/watch?v=GBLuOH2_tFM",
@@ -163,6 +185,11 @@ while True:
                 units.unit5.video6.captions, units.unit5.video7.captions, units.unit5.video8.captions, units.unit5.video9.captions, units.unit5.video10.captions,
                 units.unit5.video11.captions, units.unit5.video12.captions, units.unit5.video13.captions, units.unit5.video14.captions, units.unit5.video15.captions]
                 note = [notesimport.unit5.ki51, notesimport.unit5.ki52, notesimport.unit5.ki53, notesimport.unit5.ki54, notesimport.unit5.ki55]
+                notelinks = ["https://docs.google.com/document/d/1QVddX8w2U8fAt4B-frqIi_HLg1hl94SSsTTvh27WukU/edit?usp=sharing",
+                "https://docs.google.com/document/d/13qCrsrokm-xodkJLgfdejFa_v7kADXjEdf1_O2NiIFY/edit?usp=sharing",
+                "https://docs.google.com/document/d/1XI2BEdyFrZcqPuNR_DXze-iiY9lwpcRBBXxaFHUTASI/edit?usp=sharing",
+                "https://docs.google.com/document/d/1G1e9U8eQCC6aES1GyxmeknBPEEj02z3gqcF4PkUq0r8/edit?usp=sharing",
+                "https://docs.google.com/document/d/1tV1vwZxnRxBdeKOAPNpacIMAQzNwF4kbWed4gMRfuOk/edit?usp=sharing"]
                 timeperiod =  '1848 - 1877'
             if unit == 'unit6':
                 videolinks = ["https://www.youtube.com/watch?v=KfGPNoOqV_U", "https://www.youtube.com/watch?v=DzEWzhPgwas", "https://www.youtube.com/watch?v=5QymghrvLiM",
@@ -174,24 +201,48 @@ while True:
                 units.unit6.video6.captions, units.unit6.video7.captions, units.unit6.video8.captions, units.unit6.video9.captions, units.unit6.video10.captions,
                 units.unit6.video11.captions, units.unit6.video12.captions, units.unit6.video13.captions]
                 note = [notesimport.unit6.ki61, notesimport.unit6.ki62, notesimport.unit6.ki63, notesimport.unit6.ki64, notesimport.unit6.ki65, notesimport.unit6.ki66, notesimport.unit6.ki67]
+                notelinks = ["https://docs.google.com/document/d/1aTJ-caO58SGH3zxTdxT3GcIDy1YijgjMM_Y5wItsMf4/edit?usp=sharing",
+                "https://docs.google.com/document/d/1MDVXLC0xsaMwq8enzoSWhqM5Gm1pMQ2xNHA7iTsb7N8/edit?usp=sharing",
+                "https://docs.google.com/document/d/13eDdsRb2Cu4QBeLMkZDoBVKzj8ep8zjsdtnlfQpukXs/edit?usp=sharing",
+                "https://docs.google.com/document/d/1C3dcR-OH7wyWgnY4qGcSVTTxzhdfDnD10jsWwkPwcvQ/edit?usp=sharing",
+                "https://docs.google.com/document/d/1F7YIZh4rG19KNFYCBZZQZtk2ijZfcQQ16UkyVTC2MYA/edit?usp=sharing",
+                "https://docs.google.com/document/d/16cDlLFPVP55_sUqzSqrw09ZpkrdoYYUZUWNDlMEynN8/edit?usp=sharing",
+                "https://docs.google.com/document/d/1azxwrZXGdkDY97Bsjy5SxlcLr3uGvOG0xI_1-mwIars/edit?usp=sharing"]
                 timeperiod =  '1877 - 1898'
             if unit == 'unit7':
                 videolinks = ["https://www.youtube.com/watch?v=IeGmfbRrJ5Y", "https://www.youtube.com/watch?v=0eT_B9g-ZaA", "https://www.youtube.com/watch?v=6cVgZsMczps",
                 "https://www.youtube.com/watch?v=NwQrJNrRj7U", "https://www.youtube.com/watch?v=PpTNdN9MnRw"]
                 subject = [units.unit7.video1.captions, units.unit7.video2.captions, units.unit7.video3.captions, units.unit7.video4.captions, units.unit7.video5.captions]
                 note = [notesimport.unit7.ki71, notesimport.unit7.ki72, notesimport.unit7.ki73, notesimport.unit7.ki74, notesimport.unit7.ki75, notesimport.unit7.ki76, notesimport.unit7.ki77, notesimport.unit7.ki78]
+                notelinks = ["https://docs.google.com/document/d/1WLx-HyC9A5LjPlc4wzuB8_OfsJnTUF-FfXR9cmkQ0F4/edit?usp=sharing",
+                "https://docs.google.com/document/d/1KYK8o0s50aW2-X-vsG6pw2uxgXWICLaKMb5enw89lJk/edit?usp=sharing",
+                "https://docs.google.com/document/d/1PIXW8T6-TkPHLn2mYf04kjy5S2q_BAaGqjtlDnkC3MU/edit?usp=sharing",
+                "https://docs.google.com/document/d/1QBr2z-qPW_UJMCBz2cil73V1UgAWQkle8-UNCegT-4o/edit?usp=sharing",
+                "https://docs.google.com/document/d/1wXf4hTE-8yVoQ67zcZyFtkHVyhGixgpBnMcdfqg53CE/edit?usp=sharing",
+                "https://docs.google.com/document/d/1Q0qSgEMj5zR4FsyVGPfNirDy5TtIqQ_e8TTjo55nOs4/edit?usp=sharing",
+                "https://docs.google.com/document/d/1UrDf7ZKXRNwvbVAQ2qP-OA7jCu500UyJsWoLSM9ognU/edit?usp=sharing",
+                "https://docs.google.com/document/d/1DKxfYhgAlaWLaz4P7sRnYaaHKM3U_3eF6g-cr5iV2D4/edit?usp=sharing"]
                 timeperiod = '1898 - 1945'
             if unit == 'unit8':
                 videolinks = ["https://www.youtube.com/watch?v=IyOs5BJ3HGo", "https://www.youtube.com/watch?v=dfLmZdq6iQo", "https://www.youtube.com/watch?v=eseJiBno8Qk"]
                 subject = [units.unit8.video1.captions, units.unit8.video2.captions, units.unit8.video3.captions]
                 note = [notesimport.unit8.ki81, notesimport.unit8.ki82, notesimport.unit8.ki83, notesimport.unit8.ki84, notesimport.unit8.ki85, notesimport.unit8.ki86]
+                notelinks = ["https://docs.google.com/document/d/1K_Je4JYVP36YCMwMKl6p8VwlDr4_TsfSwIsvQzpR74M/edit?usp=sharing",
+                "https://docs.google.com/document/d/157x_QdGhUmVOdYcXPmXA_sbhn49euaT6R15HsvrZudA/edit?usp=sharing",
+                "https://docs.google.com/document/d/1JdKQrEbaN9OJhlpgF1GarnBkETH8imPGEm5mgrwGI20/edit?usp=sharing",
+                "https://docs.google.com/document/d/1zLGpzp_Y5A4BGXu44YW7zGl4SoTfO6m9UrMvHBY6rCU/edit?usp=sharing",
+                "https://docs.google.com/document/d/1G3DQBvpatxD5gYlxN6FNN_6MqCUmRfQdcX6hLUK3FmM/edit?usp=sharing",
+                "https://docs.google.com/document/d/18qvjbSUd_xPH39k6AXIjw9WUtPNDotSxdx8Li0r-cD8/edit?usp=sharing"]
                 timeperiod =  '1945 - 1980'
             if unit == 'unit9':
                 videolinks = ["https://www.youtube.com/watch?v=WzqeAXBwzqc", "https://www.youtube.com/watch?v=gvREnUWMKoU", "https://www.youtube.com/watch?v=LOJpTzjU7xc"]
                 subject = [units.unit9.video1.captions, units.unit9.video2.captions, units.unit9.video3.captions]
                 note = [notesimport.unit9.ki91, notesimport.unit9.ki92, notesimport.unit9.ki93]
+                notelinks = ["https://docs.google.com/document/d/1_Ccdsy8Zgd_P4MRR3HkLdOjKp2s7NukUJGRmfA3Xx24/edit?usp=sharing",
+                "https://docs.google.com/document/d/1H4mpb_RBoElM_yGvgP3HCSOZ9eiz0yTcrJf3vBBI4F4/edit?usp=sharing",
+                "https://docs.google.com/document/d/1T6nZPZlofimuo8FOrkjJBNKU6XD7a-NxAc97Bfh9DWc/edit?usp=sharing"]
                 timeperiod =  '1980 - Present'
-            return videolinks, subject, note, timeperiod
+            return videolinks, subject, note, notelinks, timeperiod
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -214,7 +265,7 @@ while True:
             if unit == None:
                 err = 'pageantry'
             else:
-                videos, subject, notes, daterange = unitdb(unit)
+                videos, subject, notes, notelinks, daterange = unitdb(unit)
 
             if err == 'nil':
                 timeperiod = daterange
@@ -233,6 +284,8 @@ while True:
                         end = int(pos[1] + 125)
 
                         content = f"...{notetext[start:end]}..."
+
+                        link = notelinks[count]
 
                         # seek the position of that text that we just displayed, return and print the number of lines from start
 
@@ -307,7 +360,7 @@ while True:
             if unit == None:
                 err = 'pageantry'
             else:
-                videos, subject, notes, daterange = unitdb(unit)
+                videos, subject, dontuse, notelinks, daterange = unitdb(unit)
 
             if err == 'nil':
                 timeperiod = daterange
@@ -499,7 +552,7 @@ while True:
             action = input("What's next? ")
         if action == "show me Mayr's notes!":
             # open the notes page
-            test = "test"
+            webbrowser.open(notes.link)
             action = input("What's next? ")
         if action == 'next!':
             #vocabresult()
